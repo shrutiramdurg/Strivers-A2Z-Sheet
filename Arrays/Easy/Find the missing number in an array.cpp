9a9d1b21a383bcd4b9c261d5b0664f7e50908b45
shrutiramdurg <shrutisramdurg@gmail.com>
2025-06-16 18:42:12 +0530

    Solved Find the missing number in an array

---
+++ Arrays/Easy/Find the missing number in an array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int>&a, int N) {
+
+    //Summation of first N numbers:
+    int sum = (N * (N + 1)) / 2;
+
+    //Summation of all array elements:
+    int s2 = 0;
+    for (int i = 0; i < N - 1; i++) {
+        s2 += a[i];
+    }
+
+    int missingNum = sum - s2;
+    return missingNum;
+}
