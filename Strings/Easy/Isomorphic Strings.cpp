3d6de17edaba62b7ea7e3c041118bd6c3e1dd415
shrutiramdurg <shrutisramdurg@gmail.com>
2025-06-20 16:01:34 +0530

    Solved Isomorphic Strings

---
+++ Strings/Easy/Isomorphic Strings.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+bool isIsomorphic(string s, string t) {
+    if (s.length() != t.length()) return false;
+
+    unordered_map<char, char> mapST, mapTS;
+
+    for (int i = 0; i < s.length(); ++i) {
+        char c1 = s[i], c2 = t[i];
+
+        
+        if (mapST.count(c1)) {
+            if (mapST[c1] != c2) return false;
+        } else {
+            mapST[c1] = c2;
+        }
+
+        
+        if (mapTS.count(c2)) {
+            if (mapTS[c2] != c1) return false;
+        } else {
+            mapTS[c2] = c1;
+        }
+    }
+
+    return true;
+}
