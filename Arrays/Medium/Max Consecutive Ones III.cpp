ddd331d118d02eface202d5a9b700557e7202180
shrutiramdurg <shrutisramdurg@gmail.com>
2025-06-24 17:55:36 +0530

    Solved Max Consecutive Ones III

---
+++ Arrays/Medium/Max Consecutive Ones III.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0, right = 0;
+        int zeroCount = 0;
+        int maxLen = 0;
+
+        while (right < nums.size()) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            // If we have more than k zeros, shrink the window
+            while (zeroCount > k) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            // Update max length
+            maxLen = max(maxLen, right - left + 1);
+            right++;
+        }
+
+        return maxLen;
+    }
+};
