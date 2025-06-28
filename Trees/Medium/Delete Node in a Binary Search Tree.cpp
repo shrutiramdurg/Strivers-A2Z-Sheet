014684a4b48e81a729867ffe37e56a4e96937d40
shrutiramdurg <shrutisramdurg@gmail.com>
2025-06-28 14:54:43 +0530

    Solved Delete Node in a Binary Search Tree

---
+++ Trees/Medium/Delete Node in a Binary Search Tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* findMin(TreeNode* node) {
+        while (node && node->left)
+            node = node->left;
+        return node;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return nullptr;
+
+        if (key < root->val)
+            root->left = deleteNode(root->left, key);
+        else if (key > root->val)
+            root->right = deleteNode(root->right, key);
+        else {
+            if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+            if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+            TreeNode* successor = findMin(root->right);
+            root->val = successor->val;
+            root->right = deleteNode(root->right, successor->val);
+        }
+
+        return root;
+    }
+};
