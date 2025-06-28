3604d7010a46a47d975df4bec063a8a30d77ad2b
shrutiramdurg <shrutisramdurg@gmail.com>
2025-06-28 16:09:26 +0530

    Solved Subarray Product Less Than K

---
+++ Arrays/Medium/Subarray Product Less Than K.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int left = 0;
+        int curr = 1;
+        int ans = 0;
+        if(k<=1){
+            return 0;
+        }
+        for(int right = 0;right<nums.size();right++){
+            curr*=nums[right];
+            while(curr>=k){
+                curr /= nums[left];
+                left++;
+            }
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+};
